f0d038c3239e966e1ea7deda892a4b1e4c4039e7
YUKIJUDAI <1787149082@qq.com>
2020-05-21 13:12:42 +0800

    init

--- src/login.tsx
+++ src/login.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { connect,Provider } from "react-redux";
+import store from "./reducer";
+import { bindActionCreators } from "redux";
 
 export default class Login extends Component {
 
@@ -24,17 +27,33 @@ export default class Login extends Component {
             window.alert("用户名或密码错误");
         } else {
             this.setState({ loginFlag: true });
+            this.props.changeUserInfo()
         }
     }
 
     render() {
         if (this.state.loginFlag) return <Redirect to={"/home"} />;
         return (
-            <form>
-                <input placeholder="请输入用户名" value={this.state.username} onChange={this.setUser.bind(this, "username")} />
-                <input placeholder="请输入密码" value={this.state.password} onChange={this.setUser.bind(this, "password")} />
-                <button onClick={this.submit}> 登录 </button>
-            </form>
+            <Provider store={store}>    
+                <form>
+                    <input placeholder="请输入用户名" value={this.state.username} onChange={this.setUser.bind(this, "username")} />
+                    <input placeholder="请输入密码" value={this.state.password} onChange={this.setUser.bind(this, "password")} />
+                    <button onClick={this.submit}> 登录 </button>
+                </form>
+            </Provider>
         );
     }
 }
+
+function changeUserInfo(){
+    return {type:"CHANGE"};
+}
+
+function mapStateToProps(state:any) { 
+    return {...state}
+};
+
+function mapDispatchToProps(dispatch:any){
+    return bindActionCreators({changeUserInfo},dispatch);
+};
+connect(mapStateToProps,mapDispatchToProps)(Login);
---
+++ src/reducer.ts
@@ -0,0 +1,23 @@
+import { combineReducers, createStore } from "redux";
+
+const loginInfo = {
+    username: "",
+    password: "",
+};
+
+function changeLoginInfo(state = loginInfo, action: any) {
+    switch (action.type) {
+        case "CHANGE":
+            return (state = action.text);
+        default:
+            return state;
+    }
+}
+
+const store = createStore(changeLoginInfo);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+export default store;
